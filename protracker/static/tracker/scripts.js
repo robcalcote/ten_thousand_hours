1e6f44bc6f4a8f6b25ddaa1097593eb43631b3e1
Rob <robcalcote@gmail.com>
2020-07-16 23:05:05 -0600

    designed the left-side navigation bar. almost completely done with that - onto the main section

--- protracker/static/tracker/scripts.js
+++ protracker/static/tracker/scripts.js
@@ -1,17 +1,26 @@
 window.onload = function() {
-    document.getElementById("admin-login").onclick = function () {
-        window.location.href = "//localhost:8000/admin";
-    };
+    
+    if (document.body.contains(document.getElementById("admin-login-button"))) {
+        document.getElementById("admin-login-button").onclick = function () {
+            window.location.href = "//localhost:8000/admin";
+        };    
+    }
 
-    if (document.body.contains(document.getElementById("user-login"))) {
-        document.getElementById("user-login").onclick = function () {
+    if (document.body.contains(document.getElementById("user-login-button"))) {
+        document.getElementById("user-login-button").onclick = function () {
             window.location.href = "//localhost:8000/tracker/login";
         };    
     }
 
-    if (document.body.contains(document.getElementById("user-logout"))) {
-        document.getElementById("user-logout").onclick = function () {
+    if (document.body.contains(document.getElementById("user-logout-button"))) {
+        document.getElementById("user-logout-button").onclick = function () {
             window.location.href = "//localhost:8000/tracker/logout";
+        };
+    }
+
+    if (document.body.contains(document.getElementById("dashboard-button"))) {
+        document.getElementById("dashboard-button").onclick = function () {
+            window.location.href = "//localhost:8000/tracker/dashboard/";
         };
     }
 };

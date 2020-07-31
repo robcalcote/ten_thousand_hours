62e7864925bdc3df109a3b5c47096198dd9b961a
Rob <robcalcote@gmail.com>
2020-07-30 21:32:14 -0600

    completed a tutorial on modals! will attempt to add modal on detail pages tomorrow for edit_<record>git add . super stoked on it

--- protracker/static/tracker/scripts.js
+++ protracker/static/tracker/scripts.js
@@ -23,6 +23,35 @@ window.onload = function() {
             window.location.href = "//localhost:8000/tracker/dashboard/";
         };
     }
+
+
+
+    // TEST MODAL CODE
+    // Get the modal
+    var modal = document.getElementById("edit-record-modal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("edit-record-button");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-modal")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
 };
 
 
